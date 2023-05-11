7fd4bff2d3950e761df884a259ca60d882390bfc
Sergey Molchanov <ser-mol2001@yandex.ru>
2023-05-11 03:36:20 +0300

    fix

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -77,22 +77,23 @@ func HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Println("Successfully parsed")
 
-	// if update.Message.Text == "/start" {
-	// 	if err = createButtons(update.Message.Chat.Id); err != nil {
-	// 		log.Printf("error creating buttons, %s\n", err.Error())
-	// 		return
-	// 	}
-	// 	log.Printf("Buttons successfully created\n")
-	// }
-
-	outputMessage := "Your message: " + update.Message.Text
-
-	// Send the punchline back to Telegram
-	var telegramResponseBody, errTelegram = sendTextToTelegramChat(update.Message.Chat.Id, outputMessage)
-	if errTelegram != nil {
-		log.Printf("got error %s from telegram, reponse body is %s", errTelegram.Error(), telegramResponseBody)
+	if update.Message.Text == "/start" {
+		if err = createButtons(update.Message.Chat.Id); err != nil {
+			log.Printf("error creating buttons, %s\n", err.Error())
+			return
+		}
+		log.Printf("Buttons successfully created\n")
 	} else {
-		log.Printf("message \"%s\" successfully sent to chat id %d\n", outputMessage, update.Message.Chat.Id)
+
+		outputMessage := "Your message: " + update.Message.Text
+
+		// Send the punchline back to Telegram
+		var telegramResponseBody, errTelegram = sendTextToTelegramChat(update.Message.Chat.Id, outputMessage)
+		if errTelegram != nil {
+			log.Printf("got error %s from telegram, reponse body is %s", errTelegram.Error(), telegramResponseBody)
+		} else {
+			log.Printf("message \"%s\" successfully sent to chat id %d\n", outputMessage, update.Message.Chat.Id)
+		}
 	}
 }
 
